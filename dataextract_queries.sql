33784477ffa60bf0b186a996e738daad2ecf3101
Rahul Jain <you@example.com>
2019-02-20 13:14:27 +0530

    StudentDetails Added

--- dataextract_queries.sql
+++ dataextract_queries.sql
@@ -29,4 +29,4 @@ from courseslist, CourseD as CD
 where CD.coursecode = courseslist.coursecode
 order by courseid;
 
-\copy (Select * FROM CourseDetails ) to 'C:/Users/rahul/IITD_TimeSchedule/CourseDetails.csv' with csv;
+\copy (Select * FROM CourseDetails ) to 'C:/Users/rahul/IITD_TimeSchedule/CourseDetails.csv' with csv delimiter '$';
